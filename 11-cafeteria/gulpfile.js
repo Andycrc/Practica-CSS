fbcef12be1933e1643179e7991f6ab1683c5fec1
Andycrc <anderson.carcamo@catolica.edu.sv>
2024-10-01 15:32:05 -0600

    subiendo cambios proyecto 11

--- 11-cafeteria/gulpfile.js
+++ 11-cafeteria/gulpfile.js
@@ -1,8 +1,51 @@
+const { src, dest, watch, series, parallel } = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
 
-function tarea( donde){
-    console.log("primera tarea");
-    donde();
+
+
+function css(done) {
+    src('src/scss/app.scss')
+        .pipe(sass().on('error', sass.logError))  // Manejo de errores en Sass
+        .pipe(postcss([autoprefixer()]))          // Autoprefixer para compatibilidad de navegadores
+        .pipe(dest('build/css'));                 // Guardar el CSS en la carpeta build
+
+    done();
 }
 
 
-exports.tarea = tarea;
+// async function imagenes(done) {
+//     const imagemin = (await import('gulp-imagemin')).default;
+    
+//     src('src/img/**/*')
+//         .pipe(imagemin({ optimizationLevel: 3 }))
+//         .pipe(dest('build/img'));
+    
+//     done();
+// }
+
+// async function versionWebp() {
+//     const webp = (await import('gulp-webp')).default;
+    
+//     return src('src/img/**/*.{png,jpg}')
+//         .pipe(webp())
+//         .pipe(dest('build/img'));
+// }
+
+
+
+
+function dev() {
+    watch('src/scss/**/*.scss', css);  // Ejecuta la tarea css al detectar cambios en SCSS
+   // watch('src/img/**/*', imagenes);
+}
+
+
+
+exports.css = css;
+exports.dev = dev;
+// exports.imagenes = imagenes;
+// exports.versionWebp = versionWebp;
+// exports.default = series(imagenes, versionWebp, css, dev);
+exports.default = series(css, dev);
